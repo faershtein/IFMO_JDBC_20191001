3ad43eed697f722b6ab24d392ed82e7e20cc04df
Daniil Faershtein <kar-faershtein@ya.ru>
2019-11-01 18:07:56 +0300

    Update SqlQueries.java

--- src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
@@ -28,8 +28,8 @@ public class SqlQueries {
     //Select all employees and corresponding department name if there is one.
     //Name column containing name of the department "depname".
     //language=HSQLDB
-    String select06 = "SELECT emp.id, FIRSTNAME, LASTNAME, SALARY, d.NAME " +
-            "from EMPLOYEE emp " + "LEFT JOIN DEPARTMENT D on emp.DEPARTMENT = D.ID";;
+    String select06 = "SELECT emp.ID, FIRSTNAME, LASTNAME, SALARY, d.NAME " +
+            "from EMPLOYEE emp " + "LEFT JOIN DEPARTMENT D on emp.DEPARTMENT = D.ID";
 
     //Select total salary pf all employees. Name it "total".
     //language=HSQLDB
