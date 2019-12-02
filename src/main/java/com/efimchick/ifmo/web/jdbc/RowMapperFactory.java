fc50dfe78634ba9acbb6c3eab273842f3848eec2
Daniil Faershtein <kar-faershtein@ya.ru>
2019-12-02 20:23:48 +0300

    Revert "first attempt"
    This reverts commit c4bb9b2a09cf8040156cee2fac36d23184191ca3.

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,40 +1,10 @@
 package com.efimchick.ifmo.web.jdbc;
 
-import com.efimchick.ifmo.web.jdbc.domain.FullName;
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
-import com.efimchick.ifmo.web.jdbc.domain.Position;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class RowMapperFactory {
+
     public RowMapper<Employee> employeeRowMapper() {
-        RowMapper<Employee> rowMapper = new RowMapper<Employee>() {
-            @Override
-            public Employee mapRow(ResultSet resultSet) {
-                try {
-                    Employee employee = new Employee(
-                            new BigInteger(String.valueOf(resultSet.getInt("id"))),
-                            new FullName(
-                            resultSet.getString("firstName"),
-                            resultSet.getString("lastName"),
-                            resultSet.getString("middleName")),
-                            Position.valueOf(resultSet.getString("position")),
-                            LocalDate.parse(resultSet.getString("hiredate")),
-                            new BigDecimal(String.valueOf(resultSet.getInt("salary"))));
-
-
-                    return employee;
-                }
-                catch (SQLException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-
-        };
-        return rowMapper;
+        throw new UnsupportedOperationException();
     }
 }
--- src/main/java/com/efimchick/ifmo/web/jdbc/domain/Employee.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/domain/Employee.java
@@ -38,9 +38,6 @@ public class Employee {
         this.salary = salary.setScale(5, RoundingMode.HALF_UP);
     }
 
-
-
-
     public BigInteger getId() {
         return id;
     }
