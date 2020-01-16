39603e1f827224cbed40190fc230250fa3743fae
faershtein <56654710+faershtein@users.noreply.github.com>
2020-01-16 21:35:57 +0300

    P3223 Faershtein Daniil task-2
    commit

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,36 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
-
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        RowMapper<Employee> ROWMAPPER = new RowMapper<Employee>() {
+            @Override
+            public Employee mapRow(ResultSet RSLT){
+                try {
+                    Employee employ = new Employee(
+                            new BigInteger(String.valueOf(RSLT.getString("id"))),
+                            new FullName(RSLT.getString("firstname"),
+                                    RSLT.getString("lastname"),
+                                    RSLT.getString("middlename")),
+                            Position.valueOf(RSLT.getString("position")),
+                            LocalDate.parse(RSLT.getString("hiredate")),
+                            new BigDecimal(String.valueOf(RSLT.getDouble("salary")))
+                    );
+                    return employ;
+                } catch (SQLException exception){
+                    exception.printStackTrace();
+                    return null;
+                }
+            }
+        };
+        return ROWMAPPER;
     }
 }
